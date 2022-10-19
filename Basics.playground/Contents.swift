f550cb175447501007731aa227f6f05a656f212a
naresh-pt6259 <nareshkumaroviliraja2002@gmail.com>
2022-10-19 17:57:06 +0530

    basics of swift2

--- Basics.playground/Contents.swift
+++ Basics.playground/Contents.swift
@@ -13,12 +13,19 @@ greeting = "The number is \(a)"
 print("\(greeting)\n")
 
 //Tuples
-var student = ("asdfg", 21,21,32)
+//var student = ("asdfg", 21,21,32,74)
+var student = (name: "asdfg", m1: 21, m2: 21, m3: 32, 74)
 
-var (name,m1,m2,m3) = student
+//var (name,m1,m2,m3, _) = student
 
 print("\(student)\n")
-print("Name: \(name)")
-print("Subject1: \(m1)")
-print("Subject2: \(m2)")
-print("Subject3: \(m3)\n")
+print("Name: \(student.name)")
+print("Subject1: \(student.m1)")
+print("Subject2: \(student.m2)")
+print("Subject3: \(student.m3)")
+print("Total: \(student.4)\n")
+
+let possibleNumber = "123"
+let convertedNumber = Int(possibleNumber)
+
+print(convertedNumber! + 1)
